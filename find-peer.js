60ae78914f7e2a935e11d178c116979c88cbd8f0
Richard Brooks <richard@bittorrent.com>
2016-01-28 22:11:00 -0800

    moar functionality

--- find-peer.js
+++ find-peer.js
@@ -7,6 +7,8 @@ var net = require('net')
 var canihazip = require('canihazip')
 var deasync = require('deasync')
 var readline = require('readline')
+var ed = require('ed25519-supercop')
+
 
 // THIS IS SUPER GROSS, I'M SORRY
 var done = false;
@@ -32,6 +34,7 @@ var PeersCollection = Backbone.Collection.extend({
 
 var Peers = new PeersCollection();
 
+/*
 Peers.on('add', function(peer) {
   var socket = net.createConnection(peer.get('port'), peer.get('host'));
   peer.set('socket', socket);
@@ -45,6 +48,7 @@ Peers.on('add', function(peer) {
     console.log('SOCKET DONE')
   });
 });
+*/
 
 var dht = new DHT()
 
@@ -54,54 +58,71 @@ dht.listen(20000, function () {
 
 cli
   .version('0.0.1')
-  .option('-s, --secret [secret]')
-  .option('-p, --port [port]')
+  .option('--seed [seed]')
+  .option('--secret [secret]')
 
 cli.parse(process.argv);
 
-var serverPort = cli.port || 31337;
-
-var server = net.createServer(function(socket) {
-  socket.write('Hello 31337 h4xor');
-
-  socket.on('data', function(data) {
-    var assumedString = data.toString();
-    if (assumedString.toUpperCase() === 'PING') {
-      socket.write('PONG');
-    } else {
-      console.log("IN:", assumedString);
-      socket.write(assumedString.toUpperCase());
-    }
-  });
-});
-server.listen(serverPort, '0.0.0.0');
+var keypair = ed.createKeyPair(ed.createSeed(cli.seed))
 
 if (cli.secret) {
-  var secret = cli.secret;
-  sha1Secret = crypto.createHash('sha1').update(secret).digest('hex');
-  console.log('secret:', sha1Secret);
-  dht.announce(sha1Secret, serverPort);
-  dht.lookup(sha1Secret);
+  var sha1Secret = cli.secret;
+  dht.get(sha1Secret, function(err, res) {
+    console.log('err:', err); 
+    console.log('res:', res);
+  });
+  //sha1Secret = crypto.createHash('sha1').update(secret).digest('hex');
 
-  dht.on('peer', function (newPeer, infoHash, from) {
-    if (newPeer.host != externalIP && newPeer.port >= 31337 && newPeer.port <= 31437 && !Peers.get(newPeer.host + ':' + newPeer.port)) {
-      console.log('Actual New Peer:', newPeer);
-      console.log('From:', from.address, from.port);
-      var newPeerModel = new Peer({host:newPeer.host, port:newPeer.port});
-      Peers.add(newPeerModel);
-      console.log('total peers', Peers.length);
-    }
-  })
+  /*
+  setInterval(function() {
+    //dht.announce(sha1Secret, serverPort);
+    dht.lookup(sha1Secret);
+  }, 300); //announce and search every 5 minutes
+  */
 }
+
+if (cli.seed) {
+}
+
+dht.on('peer', function (newPeer, infoHash, from) {
+  if (newPeer.host != externalIP && newPeer.port >= 31337 && newPeer.port <= 31437 && !Peers.get(newPeer.host + ':' + newPeer.port)) {
+    console.log('Actual New Peer:', newPeer);
+    console.log('From:', from.address, from.port);
+    var newPeerModel = new Peer({host:newPeer.host, port:newPeer.port});
+    Peers.add(newPeerModel);
+    console.log('total peers', Peers.length);
+  }
+})
 
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+var opts = {
+  seq: 0
+};
+
 rl.on('line', function(line){
-  Peers.each(function(peer) {
-    var peerSocket = peer.get('socket');
-    peerSocket.write(line);
-  });
+  if (line[0] == '$') {
+    eval(line.substring(1));
+  } else {
+    if (cli.seed) {
+      // Announce DHT put update
+      var value = new Buffer(200).fill(line)
+      opts = {
+        k: keypair.publicKey,
+        seq: 0 || opts.seq++,
+        v: value,
+        sign: function (buf) {
+          return ed.sign(buf, keypair.publicKey, keypair.secretKey)
+        }
+      }
+
+      dht.put(opts, function (err, hash) {
+        console.error('error=', err)
+        console.log('hashDigest=', hash.toString('hex'))
+      }) 
+    }
+  }
 });
